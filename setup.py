6bf89948b122b2a4964c47147500d86a0c09d85f
Jean-Lou Dupont <github@jldupont.com>
2010-08-01 12:24:13 -0400

    0.3

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+    Setup script for 'transmission-helper'
+
+    @author: Jean-Lou Dupont
+"""
+__author__  ="Jean-Lou Dupont"
+__version__ ="0.3"
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name=         'transmission-helper',
+      version=      __version__,
+      description=  'Helper scripts for BT Transmission',
+      author=       __author__,
+      author_email= 'jl@jldupont.com',
+      url=          'http://www.systemical.com/',
+      package_dir=  {'': "src",},
+      packages=     ["transmission_helper"],
+      scripts=      ['src/scripts/trns-remove', ],
+      )
